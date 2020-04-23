a981792a6bd68c9306b757fab4d37e34d79a20e2
Scott Singer <ssinger303@gmail.com>
2020-04-23 20:57:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ return `Happy holidays, ${name}!`
 }
 
 function happyCustomHolidayTo(holiday, name){
-    return `Happy ${holiday}, ${name}!`
+    return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days){
-  return `It's ${days} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`;
 }
